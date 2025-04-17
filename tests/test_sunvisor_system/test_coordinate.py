0a93a0d2fe1a53e448e8d0701c02da08f1d76b5e
MeiLL3r <meiller0526@naver.com>
2025-04-18 00:13:52 +0900

    test: 시각화 함수에 대한 테스트 코드 추가 및 상수값 수정

--- tests/test_sunvisor_system/test_coordinate.py
+++ tests/test_sunvisor_system/test_coordinate.py
@@ -1,9 +1,11 @@
 import unittest
 
+from src.sunvisor_system.mapping import coordinates
+from src.sunvisor_system.mapping.config import DEFAULT_IMAGE_SIZE
+from src.sunvisor_system.visualize.grid_display import visualize_grid_image
+
+
 # 테스트를 위해 get_grid_size 함수를 덮어씁니다.
-from sunvisor_system.mapping import coordinates
-
-
 # get_grid_size를 테스트에서 고정된 값으로 설정 (예: 100 열, 50 행)
 def dummy_get_grid_size():
     return (100, 50)
@@ -19,7 +21,8 @@ class TestCameraToDriverCoords(unittest.TestCase):
         카메라 이미지 중앙에 위치한 태양 (픽셀 좌표 (320,240))이
         운전자 시점으로 변환될 때, 예상되는 그리드 좌표는 (50, 0)
         """
-        sun_center = (320, 240)  # 이미지 중앙
+        x, y = DEFAULT_IMAGE_SIZE
+        sun_center = (x / 2, y / 2)  # 이미지 중앙
         expected = (50, 0)
         result = coordinates.camera_to_driver_coords(sun_center)
         self.assertEqual(result, expected)
@@ -39,10 +42,18 @@ class TestCameraToDriverCoords(unittest.TestCase):
         이미지 우측 하단 (픽셀 좌표 (640,480))에 태양이이
         운전자 시점으로 변환될 때, 예상되는 그리드 좌표는 (99, 0)
         """
-        sun_center = (640, 480)  # 오른쪽 하단
+        x, y = DEFAULT_IMAGE_SIZE
+        sun_center = (x, y)  # 오른쪽 하단
         result = coordinates.camera_to_driver_coords(sun_center)
         expected = (99, 0)
         self.assertEqual(result, expected)
+
+    def test_visualize(self):
+        x, y = DEFAULT_IMAGE_SIZE
+        sun_center = (x / 2, y / 2)  # 이미지 중앙
+        result = coordinates.camera_to_driver_coords(sun_center)
+        print(f"Grid Position: {result}")
+        visualize_grid_image(result)
 
 
 if __name__ == "__main__":
